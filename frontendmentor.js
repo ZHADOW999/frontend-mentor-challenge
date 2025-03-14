40b90c973b889bdd7b2056023161f8afefef1742
ZHADOW999 <babalolav9@gmail.com>
2025-03-14 14:44:37 +0100

    Refactor styles and scripts: remove unused files and update general.css for layout adjustments

--- frontendmentor.js
+++ frontendmentor.js
@@ -1,13 +1,26 @@
-const dropdownBtns = document.querySelectorAll('.dropdownBtn');
+const dropdownBtns = document.querySelectorAll('.dropdownclass');
 for(const dropdownBtn of dropdownBtns){
   dropdownBtn.addEventListener('click', function(){
     const dropDown = this.querySelector('.dropdown');
     dropDown.classList.toggle('show');
-    dropDown.classList.toggle('dropdown-1');
-    dropDown.classList.toggle('dropdown-2');
+    // dropDown.classList.toggle('dropdown-1');
+    // dropDown.classList.toggle('dropdown-2');
     dropDown.classList.toggle('dropdown-mobile');
   });
 }
+
+
+const burgerMenu = document.querySelector('.menu-icon');
+const mobileDropdown = document.querySelector('.mobile-view-dropdown');
+const closeMenu = document.querySelector('.cross-mobile');
+
+burgerMenu.addEventListener('click', function () {
+  mobileDropdown.classList.toggle('show');
+});
+
+closeMenu.addEventListener('click', function () {
+  mobileDropdown.classList.remove('show');
+});
 /*const burgermenu = document.querySelector('.menu-icon');
 for(const burgermenu of buttons){
   dropdownBtn.addEventListener('click', function(){
